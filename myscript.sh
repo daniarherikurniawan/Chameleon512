bd09a380d238fa032ee0c9abff373512b0ae270b
Daniar Heri Kurniawan <ddhhkk2@gmail.com>
2017-04-19 02:00:14 +0700

    update script

--- myscript.sh
+++ myscript.sh
@@ -23,47 +23,11 @@ echo
 echo ================================================
 echo  
 
-git add --all .
-git commit -m "update the latest code" -a
-git push origin master
-echo Finished updating git codes!
 
-projURI=hadoopcluster.cs331-uc.emulab.net
+# host=node-0.$projURI
 
 
-counter=0
-while [ $counter -lt $maxNodes ]
-do
-	host=node-$counter.$projURI
-	echo 
-	echo node: $host
-	echo ...... START Scanning ssh fingerprint 
-	ssh-keygen -R $host
-	ssh-keyscan $host >> ~/.ssh/known_hosts 
-	echo ...... END Scanning ssh fingerprint 
-
-	((counter++))
-done
-
-
-counter=0
-while [ $counter -lt $maxNodes ]
-do
-	host=node-$counter.$projURI
-	if [ $counter -eq 0 ]
-	then
-		ssh $host 'bash -s' < masterscript.sh $maxNodes
-	else
-		ssh $host 'bash -s' < slavescript.sh $maxNodes 
-	fi
-
-	((counter++))
-done
-
-host=node-0.$projURI
-
-
-ssh $host 'bash -s' < startNNDN.sh
+# ssh $host 'bash -s' < startNNDN.sh
 
 echo ...... Finished Starting DN and NN
 
@@ -73,7 +37,7 @@ echo ...... Starting $numThreads threads of HDFS write using copyFromLocal
 counter=0
 while [ $counter -lt $numThreads ]
 do
-	host=node-$counter.$projURI
+	host=node-$counter
 	(echo "output from $host"; ssh $host 'bash -s' < mapredscript.sh $counter$hdfsFolder $numCopy) &
 	((counter++))
 done
@@ -81,11 +45,6 @@ done
 wait
 echo All subshells finished
 
-host=node-0.$projURI
-
-ssh $host 'bash -s' < "eraseNNLog.sh"
-
-echo Erasing namenode logs is done
 echo 	$host:50070 
 echo 	$host:50030 
 
