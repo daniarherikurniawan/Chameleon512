ab8fe17ee56309ba2df7b7f009becf28d5c5b29d
Daniar Heri Kurniawan <ddhhkk2@gmail.com>
2017-04-19 18:40:52 +0700

    update script

--- blockwrittingprogress.sh
+++ blockwrittingprogress.sh
@@ -1,3 +1,4 @@
 # sudo apt-get install w3m
+wget node-0.ucare.edu:50070/dfshealth -O index.html
 wget node-0.ucare.edu:50070/dfsnodelist.jsp?whatNodes=LIVE -O index.html
 w3m -dump index.html | less
